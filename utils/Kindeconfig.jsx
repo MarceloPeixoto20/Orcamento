f77043b18103c10fab307cfefdb42aaf1e73a6b9
Marcelo Peixoto <marcelo_filho7@hotmail.com>
2024-05-23 01:24:20 -0300

    Auntenticaçao teste

--- utils/Kindeconfig.jsx
+++ utils/Kindeconfig.jsx
@@ -4,4 +4,4 @@ export const client = new KindeSDK(
     KINDE_ISSUER_URL="https://testeestacio.kinde.com",
     KINDE_POST_CALLBACK_URL="exp://127.0.0.1:8081",
     KINDE_POST_LOGOUT_REDIRECT_URL="exp://127.0.0.1:8081",
-    KINDE_CLIENT_ID="95fc4ba8171a4e4086f80abfd8bbc8bf")
+    KINDE_CLIENT_ID="4758f3abb79748c993f4947a134b51ab")
